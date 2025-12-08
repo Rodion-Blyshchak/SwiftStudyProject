ac45090ba3a973cc657d98ce97906af0035b6624
Rodion Blyshchak <rodion.blyshchak.work@gmail.com>
2025-12-08 22:58:46 +0200

    question about NewTaskInputView

---
+++ SwiftStudyProject/Lesson7/MainVeloceViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainVeloceViewController.swift
+//  SwiftStudyProject
+//
+//  Created by Rodion Blyshchak on 08.12.2025.
+//
+
+import UIKit
+
+
+class MainVeloceViewController: UIViewController {
+	
+}
--- SwiftStudyProject/ViewController.swift
+++ SwiftStudyProject/ViewController.swift
@@ -8,26 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+	private var mainVeloceViewController = NewTaskInputView()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
 		//	ViewLesson1.shared.getRandomValue()
 		//	ViewLesson2.shared.studentAndClassResults()
 		//	ViewLesson3.shared.vehicleSimulatorAndStatics()
 		//	ViewLesson4.shared.start()
 
+		setupMainView()
 	}
 	
-	@IBAction func StoryboardViewController(_ sender: Any) {
-		let storyboard = UIStoryboard(name: "StoryboardTableViewController", bundle: nil)
-		let viewController = storyboard.instantiateViewController(identifier: "StoryboardTableViewController") as! StoryboardTableViewController
-		navigationController?.pushViewController(viewController, animated: true)
+	private func setupMainView() {
+		view.addSubview(mainVeloceViewController)
+		mainVeloceViewController.translatesAutoresizingMaskIntoConstraints = false
+		mainVeloceViewController.backgroundColor = .appGreen
+		
+		NSLayoutConstraint.activate([
+			mainVeloceViewController.topAnchor.constraint(equalTo: view.topAnchor),
+			mainVeloceViewController.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			mainVeloceViewController.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			mainVeloceViewController.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+		])
 	}
 	
-	@IBAction func ConstrainsViewController(_ sender: Any) {
-		let viewController = ConstraintsTableViewController()
-		navigationController?.pushViewController(viewController, animated: true)
-	}
+//	@IBAction func StoryboardViewController(_ sender: Any) {
+//		let storyboard = UIStoryboard(name: "StoryboardTableViewController", bundle: nil)
+//		let viewController = storyboard.instantiateViewController(identifier: "StoryboardTableViewController") as! StoryboardTableViewController
+//		navigationController?.pushViewController(viewController, animated: true)
+//	}
+//	
+//	@IBAction func ConstrainsViewController(_ sender: Any) {
+//		let viewController = ConstraintsTableViewController()
+//		navigationController?.pushViewController(viewController, animated: true)
+//	}
 	
 }
